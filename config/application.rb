8c29bbc9cff33a1dc3a653cebcb8f55e380a1da9
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-08-02 10:48:06 +0900

    Define application title

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module OgiharaRyo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.title = 'Ogihara Ryo'
+
     config.generators do |g|
       g.assets false
       g.helper false
