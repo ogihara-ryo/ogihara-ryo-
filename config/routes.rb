4b53844c2efca3f5a499b7215e54ae1840ffeeba
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-08-14 00:13:10 +0900

    Add blog from scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 # == Route Map
 #
-# Prefix Verb URI Pattern Controller#Action
-#   root GET  /           welcome#index
+#    Prefix Verb   URI Pattern               Controller#Action
+#     blogs GET    /blogs(.:format)          blogs#index
+#           POST   /blogs(.:format)          blogs#create
+#  new_blog GET    /blogs/new(.:format)      blogs#new
+# edit_blog GET    /blogs/:id/edit(.:format) blogs#edit
+#      blog GET    /blogs/:id(.:format)      blogs#show
+#           PATCH  /blogs/:id(.:format)      blogs#update
+#           PUT    /blogs/:id(.:format)      blogs#update
+#           DELETE /blogs/:id(.:format)      blogs#destroy
+#      root GET    /                         welcome#index
 #
 
 Rails.application.routes.draw do
   root 'welcome#index'
+  resources :blogs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314104450) do
+ActiveRecord::Schema.define(version: 20160813150359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blog_categories", force: :cascade do |t|
-    t.integer  "sort_order"
-    t.string   "permalink"
-    t.string   "name"
-    t.integer  "parent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["parent_id"], name: "index_blog_categories_on_parent_id", using: :btree
-  end
-
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
-    t.text     "content"
-    t.integer  "category_id"
-    t.boolean  "draft",       default: true, null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.index ["category_id"], name: "index_blogs_on_category_id", using: :btree
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
