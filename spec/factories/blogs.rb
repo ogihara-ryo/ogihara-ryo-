3a11c7042637f131aff8d09dd3351b4734af7b8e
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-08-14 04:49:21 +0900

    Add schema information to blogs factory from annotate

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :blog do
     sequence(:title) { |i| "title_#{i}" }
