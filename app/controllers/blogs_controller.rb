1dba5f54f38eb92cb53292aeedd42f3399a03c35
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-08-14 23:53:24 +0900

    Use percent notation

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: %i(show edit update destroy)
 
   # GET /blogs
   def index
