c812789589dd4b0cd0bb479f13ebb21a58978cfe
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-08-15 00:15:10 +0900

    Fix coding style to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
 Blog.delete_all
 Blog.connection.execute(%|SELECT setval ('blogs_id_seq', 1, false)|)
-
-1.upto(3).each do |i|
-  Blog.create(
-    title: "title_#{i}",
-    body: "body_#{i}"
-  )
-end
+1.upto(3).each { |i| Blog.create(title: "title_#{i}", body: "body_#{i}") }
